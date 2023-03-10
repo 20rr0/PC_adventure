94540c1551531ada8b86839c056b6cc8a2820b2a
Ricardo Creemers <r.creemers@gmail.com>
2023-03-10 16:31:48 +0100

    1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 document.getElementById('gameScreen').onclick = function (e) {
     // e = Mouse click event.
-    var rect = e.target.getBoundingClientRect();
+    var box = document.getElementById('gameScreen');
+    var rect = box.getBoundingClientRect();
     var x = e.clientX - rect.left; //x position within the element.
     var y = e.clientY - rect.top;  //y position within the element.
-    console.log("Left? : " + x + " ; Top? : " + y + ".");
+    //console.log("Left? : " + x + " ; Top? : " + y + ".");
     document.getElementById("character").style.left = x + "px";
     document.getElementById("character").style.top = y + "px";
 
+}
+
+document.getElementById("door").addEventListener("click", showMessage);
+
+function showMessage() {
+    console.log(target);
+    document.getElementById("charcterMessage").style.display = "block";
 }
